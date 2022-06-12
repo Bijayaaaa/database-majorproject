fec67bd7affa0f115169c7ae570b34c82cd2a269
hustleer <amaryesh123456@gmail.com>
2022-06-13 02:44:17 +0545

    more copy paste

--- engine/engine.py
+++ engine/engine.py
@@ -180,17 +180,98 @@ class searcher:
                 clauselist += 'w%d.wordid=%d' % (tablenumber, wordid)
                 tablenumber += 1
         # Create the query from the separate parts
-        fullquery = 'select %s from %s where %s' % (
-            fieldlist, tablelist, clauselist)
+        fullquery = 'select %s from %s where %s' % (fieldlist, tablelist, clauselist)
         cur = self.con.execute(fullquery)
         rows = [row for row in cur]
         return rows, wordids
+    def getscoredlist(self,rows,wordids):
+        # context-based ranking
+        totalscores=dict([(row[0],0) for row in rows])
+        # This is where you'll later put the scoring functions
+        weights=[]
+        for (weight,scores) in weights:
+            for url in totalscores:
+                totalscores[url]+=weight*scores[url]
+        return totalscores
+    def geturlname(self,id):
+        # context-based ranking
+        return self.con.execute(
+            "select url from urllist where rowid=%d" % id).fetchone( )[0]
+    def query(self,q):
+        rows,wordids=self.getmatchrows(q)
+        scores=self.getscoredlist(rows,wordids)
+        rankedscores=sorted([(score,url) for (url,score) in scores.items( )],reverse=1)
+        for (score,urlid) in rankedscores[0:10]:
+            print ('%f\t%s' % (score,self.geturlname(urlid)))
+    
+    def normalizescores(self,scores,smallIsBetter=0):
+        vsmall=0.00001 # Avoid division by zero errors
+        if smallIsBetter:
+            minscore=min(scores.values())
+            return dict([(u,float(minscore)/max(vsmall,l)) for (u,l) in scores.items()])
+        else:
+            maxscore=max(scores.values())
+            if maxscore==0: maxscore=vsmall
+            return dict([(u,float(c)/maxscore) for (u,c) in scores.items()])
 
+    def frequencyscore(self,rows):
+        counts=dict([(row[0],0) for row in rows])
+        for row in rows: counts[row[0]]+=1
+        return self.normalizescores(counts)
 
+    def locationscore(self,rows):
+        locations=dict([(row[0],1000000) for row in rows])
+        for row in rows:
+            loc=sum(row[1:])
+            if loc<locations[row[0]]: locations[row[0]]=loc
+        
+        return self.normalizescores(locations,smallIsBetter=1)
+
+    def distancescore(self,rows):
+        # If there's only one word, everyone wins!
+        if len(rows[0])<=2: return dict([(row[0],1.0) for row in rows])
+
+        # Initialize the dictionary with large values
+        mindistance=dict([(row[0],1000000) for row in rows])
+
+        for row in rows:
+            dist=sum([abs(row[i]-row[i-1]) for i in range(2,len(row))])
+            if dist<mindistance[row[0]]: mindistance[row[0]]=dist
+        return self.normalizescores(mindistance,smallIsBetter=1)
+
+    def inboundlinkscore(self,rows):
+        uniqueurls=dict([(row[0],1) for row in rows])
+        inboundcount=dict([(u,self.con.execute('select count(*) from link where toid=%d' % u).fetchone()[0]) for u in uniqueurls])   
+        return self.normalizescores(inboundcount)
+
+    def linktextscore(self,rows,wordids):
+        linkscores=dict([(row[0],0) for row in rows])
+        for wordid in wordids:
+            cur=self.con.execute('select link.fromid,link.toid from linkwords,link where wordid=%d and linkwords.linkid=link.rowid' % wordid)
+            for (fromid,toid) in cur:
+                if toid in linkscores:
+                    pr=self.con.execute('select score from pagerank where urlid=%d' % fromid).fetchone()[0]
+                    linkscores[toid]+=pr
+        maxscore=max(linkscores.values())
+        normalizedscores=dict([(u,float(l)/maxscore) for (u,l) in linkscores.items()])
+        return normalizedscores
+
+    def pagerankscore(self,rows):
+        pageranks=dict([(row[0],self.con.execute('select score from pagerank where urlid=%d' % row[0]).fetchone()[0]) for row in rows])
+        maxrank=max(pageranks.values())
+        normalizedscores=dict([(u,float(l)/maxrank) for (u,l) in pageranks.items()])
+        return normalizedscores
+
+    def nnscore(self,rows,wordids):
+        # Get unique URL IDs as an ordered list
+        urlids=[urlid for urlid in dict([(row[0],1) for row in rows])]
+        nnres=mynet.getresult(wordids,urlids)
+        scores=dict([(urlids[i],nnres[i]) for i in range(len(urlids))])
+        return self.normalizescores(scores)
 '''
 import engine
 reload(hengine)
-e=engine.searcher('searchindex.db')
+e=searcher('searchindex.db')
 e.getmatchrows('functional programming')
 '''
 
@@ -208,22 +289,11 @@ e.getmatchrows('functional programming')
 import engine
 # pagelist=['http://kiwitobes.com/wiki/Perl.html']
 pagelist=['https://en.wikipedia.org/wiki/Programming_language']
-crawler=engine.Crawler('')
 
-
-
-crawler.crawl(pagelist)
-
-
-crawler=engine.crawler('searchindex.db')
-crawler.createindextables()
-
-# index pages
-import engine
 crawler=engine.Crawler('searchindex.db')
-pages= \
-['https://en.wikipedia.org/wiki/Programming_language']
-crawler.crawl(pages)
+crawler.createindextables()     # creates index tables
+
+crawler.crawl(pagelist)         # crawls pages
 
 # checking the entries for a word by querying the database
 [row for row in crawler.con.execute(
@@ -244,6 +314,11 @@ e.getmatchrows('functional programming')
 
 
 reload(searchengine)
->> e=searchengine.searcher('searchindex.db')
->> e.getmatchrows('functional programming')
+e=engine.searcher('searchindex.db')
+e.getmatchrows('functional programming')
+
+# context based search
+import engine
+e=engine.searcher('searchindex.db')
+e.query('functional programming')
 '''
